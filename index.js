7325d1374d8c053082034aaa1ab98d108fe54c99
Kauã Maia Cousillas <kaua.cousillas03@gmail.com>
2021-04-01 15:05:33 -0300

    Aula de 01/abr/2021

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var cookieParser = require("cookie-parser"); //recebe cookies e faz o parse para
 var path = require("path");
 
 app.use(cookieParser()); //inicializa o cookie-parser
-app.use(bodyParser.json()); //inicializa, em json, o body-parser
+app.use(bodyParser.json()); //inicializa o body-parser em json
 app.use(bodyParser.urlencoded({extended:false})); //define o body-parser como não estendido
 
 app.set("view engine", "ejs"); //define a view engine do servidor como o ejs
@@ -19,27 +19,43 @@ app.set("view engine", "ejs"); //define a view engine do servidor como o ejs
 app.use(express.static(path.join(__dirname, "public"))); //define a localização da pasta estática do servidor
 
 //fim da configuração inicial do servidor /\
+//listar todos os dados \/
 app.get('/', function(req, res){
     //res.send("Oi mundo.");
-    res.render("formulario.ejs");
+    res.render("list.ejs");
 });
 
+//listar os dados de acordo com um filtro
+app.post('/', function(req, res){
+    res.render("list.ejs");
+});
+
+//cadastrar uma nova entrada; inicializar a tela add em branco
 app.get('/add', function(req, res){
     res.render("add.ejs");
 });
 
+//salvar os dados adicionados no BD
+app.post('/add', function(req, res){
+    res.render('add.ejs');
+});
+
+//editar uma entrada já existente; inicializar a tela edit em branco
 app.get('/edit', function(req, res){
     res.render("edit.ejs");
 });
 
-app.get('/list', function(req, res){
-    res.render("list.ejs");
+//salvar os dados editados no BD
+app.post('/edit', function(req, res){
+    res.render('edit.ejs');
 });
 
-app.post('/', function(req, res){
-    res.send("Oi mundo, via Post!");
+//deletar uma entrada no BD
+app.get('/del', function(req, res){
+    res.render('list.ejs');
 });
 
+//créditos
 app.get('/autor', function(req, res){
     res.send("Desenvolvido por Kauã Maia Cousillas, na disciplina de Programação III")
 });
